f069202adc6a6104c71a6963d99e406a7f7dd8ca
yangyandong <yandong.yang02@outlook.com>
2018-07-15 22:20:24 +0800

    v2,use para to light led

---
+++ 004_led_para/led.c
@@ -0,0 +1,30 @@
+
+void delay(volatile int d)
+{
+	while (d--);
+}
+
+int led_on(int which)
+{
+	unsigned int *pGPFCON = (unsigned int *)0x56000010;
+	unsigned int *pGPFDAT = (unsigned int *)0x56000014;
+
+	if (which == 4)
+	{
+		/* GPB5 */
+		*pGPFCON = 0x400;
+	}
+	else if (which == 5)
+	{
+		/* GPB6 */
+		*pGPFCON = 0x1000;
+	}
+	
+	/* …Ë÷√GPF4/5 ‰≥ˆ0 */
+	*pGPFDAT = 0;
+
+	return 0;
+}
+
+
+
