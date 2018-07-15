0698c95b0edc9e9c5e69e6c7ce3b939fcdf5c335
yangyandong <yandong.yang02@outlook.com>
2018-07-15 22:09:22 +0800

    v1,use c to light led

---
+++ 002_led_on/led_on.h
@@ -0,0 +1,11 @@
+
+
+#ifndef  _LED_ON_H
+#define  _LED_ON_H
+
+
+/*led contr reg*/
+#define GPBCON  (*(volatile unsigned long *)0x56000010)
+#define GPBDAT  (*(volatile unsigned long *)0x56000014)
+#endif
+
